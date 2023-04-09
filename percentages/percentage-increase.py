dcc189c9558e2876d66be0a02a462dd1d8f80163
mounceryan <124623846+mounceryan@users.noreply.github.com>
2023-04-09 17:02:58 +0100

    Added increase and decrease.

---
+++ percentages/percentage-increase.py
@@ -0,0 +1,10 @@
+print("This is for calculating the percentage increase between x and y.")
+
+print("Starting value: ") 
+x = int(input())
+print("Final value: ") 
+y = int(input())
+
+percentage_increase = ((y-x)/x)*100
+
+print(percentage_increase)
