70387c5009a311026757a054d095627c04bb5699
mounceryan <124623846+mounceryan@users.noreply.github.com>
2023-04-09 16:27:23 +0100

    Changed filing. Added more percentage maths.

---
+++ percentages/percentage-change.py
@@ -0,0 +1,12 @@
+# Note that percentage change is not the same as percentage difference.
+
+print("This is for calculating the percentage change between x and y.")
+
+print("First number: ") 
+x = int(input())
+print("Second number: ") 
+y = int(input())
+
+percentage_change = ((y-x)/x)*100
+
+print(percentage_change)
---
+++ percentages/percentage-of-x.py
@@ -0,0 +1,11 @@
+print("This is for calculating what x percent of y is.")
+print("Make sure to enter numbers and not any percentage symbols.")
+
+print("What is ") 
+x = int(input())
+print("of ") 
+y = int(input())
+
+percentage_of = (x)/100*y
+
+print(percentage_of)
