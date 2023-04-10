23df3142ee8092b89b46500587a418f6f7441281
mounceryan <124623846+mounceryan@users.noreply.github.com>
2023-04-10 14:17:36 +0100

    Added primes.

---
+++ percentages/x-what-percent-of-y.py
@@ -0,0 +1,12 @@
+print("This is for calculating x is what percent of y.")
+print("Make sure to enter numbers and not any percentage symbols.")
+print("The format is [x] is what percent of [y].")
+
+print("Enter number: ") 
+x = int(input())
+print("is what percent of ") 
+y = int(input())
+
+what_percent_of = (x/y)*100
+
+print(what_percent_of)
