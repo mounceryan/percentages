e9519c0ac175aa9eab66a54976ed04b30e22f1ff
mounceryan <124623846+mounceryan@users.noreply.github.com>
2023-04-12 17:25:27 +0100

    Added a NumPy folder.

---
+++ numpy/numpy-introduction.py
@@ -0,0 +1,17 @@
+# Imports the NumPy Python library.
+import numpy
+
+# Defines 'arr' as a NumPy array from 1 to 5 incluive.
+arr = numpy.array([1, 2, 3, 4, 5])
+
+# Prints 'arr'.
+print(arr)
+
+# Imports the NumPy Python library using the alias 'np'. 'np' is literally just an easier way to refer to NumPy. The alias can be anything.
+import numpy as np
+
+# Defines 'arr' as a NumPy array from 1 to 5 incluive.
+arr = np.array([1, 2, 3, 4, 5])
+
+# Prints 'arr'.
+print(arr)
