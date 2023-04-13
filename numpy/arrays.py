61f6711d1cf9b9f9279c20f388eabac506303e94
mounceryan <124623846+mounceryan@users.noreply.github.com>
2023-04-13 17:05:24 +0100

    Further arrays.

--- numpy/arrays.py
+++ numpy/arrays.py
@@ -8,4 +8,90 @@ arr = np.array([1, 2, 3, 4, 5])
 print(arr)
 
 # Prints the type of object.
-print(type(arr)
+print(type(arr))
+
+
+
+# Imports the NumPy Python library using the alias 'np'.
+import numpy as np
+
+# This time we use a tuple to create the array.
+arr = np.array((1, 2, 3, 4, 5))
+
+# Prints 'arr'.
+print(arr)
+
+
+
+# Imports the NumPy Python library using the alias 'np'.
+import numpy as np
+
+# The below creates a 0-D array a.k.a. a Scalar.
+arr = np.array(42)
+
+# Prints 'arr'.
+print(arr)
+
+
+
+# Imports the NumPy Python library using the alias 'np'.
+import numpy as np
+
+# The below creates a 1-D array a.k.a. uni-dimensional array.
+arr = np.array([1, 2, 3, 4, 5])
+
+# Prints 'arr'.
+print(arr)
+
+
+
+# Imports the NumPy Python library using the alias 'np'.
+import numpy as np
+
+# The below creates a 2-D array.
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Prints 'arr'.
+print(arr)
+
+
+
+# Imports the NumPy Python library using the alias 'np'.
+import numpy as np
+
+# The below creates a 3-D array with two 2-D arrays.
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+# Prints 'arr'.
+print(arr)
+
+
+
+# Imports the NumPy Python library using the alias 'np'.
+import numpy as np
+
+# Various arrays are below:
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+# 'ndim' returns an integer which tells us how many dimensions an array has.
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
+
+
+
+# Imports the NumPy Python library using the alias 'np'.
+import numpy as np
+
+# The below creates an array. 'ndmin defines the number of dimensions.
+arr = np.array([1, 2, 3, 4], ndmin=5)
+
+# Prints 'arr'.
+print(arr)
+
+# Prints the number of dimensions in the array.
+print('number of dimensions :', arr.ndim)
